08e128d3bba8c83b2342615d20fed12c7beb853f
wild-marten <wildmarten00@gmail.com>
2020-06-15 02:44:02 +0200

    Bug fix

--- rzymskie/rzymskie.cpp
+++ rzymskie/rzymskie.cpp
@@ -6,23 +6,18 @@
 
 using namespace std;
 
-const vector<pair<int, string>> rzym = 
-{{1000, "M"}, {900, "CM"}, {500, "D"}, 
-{400, "CD"}, {100, "C"}, {90, "XC"}, 
-{50, "L"}, {40, "XL"}, {10, "X"}, 
-{9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+const vector<pair<int, string>> rzym =
+    {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, 
+    {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
 
 string bin2rom(int x)
 {
-    if(x < 1 or x > 3999)
-        throw invalid_argument("Number out of range");
-
     string romNum;
 
-    int j=0;
-    while(x > 0)
+    int j = 0;
+    while (x > 0)
     {
-        while(x - rzym[j].first >= 0)
+        while (x - rzym[j].first >= 0)
         {
             x -= rzym[j].first;
             romNum += rzym[j].second;
@@ -32,21 +27,33 @@ string bin2rom(int x)
     return romNum;
 }
 
+bool isNumber(string x)
+{
+    for (auto e : x)
+        if (e > '9' || e < '0')
+            return false;
+
+    return true;
+}
+
+bool inRange(int x){return x >= 1 and x <= 3999;}
+
 int main(int argc, char **argv)
 {
-    try
-    {
-        if(argc == 1)
-            throw invalid_argument("Run program with at least one parameter");
+    if (argc == 1)
+        throw invalid_argument("Run program with at least one parameter");
 
-        for(int i = 1; i < argc; i++)
+    for (int i = 1; i < argc; i++)
+        if (isNumber(argv[i]))
         {
-            string str = bin2rom(stoi(argv[i]));
-            cout << str <<endl;
+            try
+            {
+                if (inRange(stoi(argv[i])))
+                {
+                    string str = bin2rom(stoi(argv[i]));
+                    cout << str << endl;
+                }
+            }
+            catch (const std::exception &e){}
         }
-    }
-    catch(const exception &e)
-    {
-        clog << e.what()<<endl;
-    }
 }
